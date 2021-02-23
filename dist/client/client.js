2ea7b7cc72f49701f7105c56e5451f23fabb8fba
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2021-02-23 21:32:28 +0100

    Stats panel added

--- dist/client/client.js
+++ dist/client/client.js
@@ -1,12 +1,13 @@
 import * as THREE from '/build/three.module.js';
 import { OrbitControls } from '/jsm/controls/OrbitControls';
+import Stats from '/jsm/libs/stats.module';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.addEventListener('change', render);
+//controls.addEventListener('change', render)
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 const cube = new THREE.Mesh(geometry, material);
@@ -19,11 +20,14 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     render();
 }
+const stats = Stats();
+document.body.appendChild(stats.dom);
 var animate = function () {
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render(scene, camera);
+    stats.update();
 };
 function render() {
     renderer.render(scene, camera);
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,6 @@
 import * as THREE from '/build/three.module.js'
 import { OrbitControls } from '/jsm/controls/OrbitControls'
+import Stats from '/jsm/libs/stats.module'
 
 const scene: THREE.Scene = new THREE.Scene()
 
@@ -10,7 +11,7 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.addEventListener('change', render)
+//controls.addEventListener('change', render)
 
 const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
 const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
@@ -28,6 +29,9 @@ function onWindowResize(){
   render()
 }
 
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
 var animate = function () {
     requestAnimationFrame(animate)
 
@@ -35,6 +39,8 @@ var animate = function () {
     cube.rotation.y += 0.01;
 
     renderer.render(scene, camera)
+
+    stats.update()
 };
 
 function render() {
